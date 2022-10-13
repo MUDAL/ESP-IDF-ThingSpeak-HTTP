3917ecf7f684dd5c1ee473fa6967149afb276d3f
Olaoluwa Raji <olaoluwaraji1999@gmail.com>
2022-10-13 17:50:07 +0100

    updates to be made

--- ESP32_Send_Data_To_ThingSpeak/include/credentials.h
+++
@@ -1,8 +0,0 @@
-#ifndef CREDENTIALS_H
-#define CREDENTIALS_H
-
-#define SSID                      "YOUR-SSID"
-#define PASS                      "YOUR-PASSWORD"
-#define WRITE_API_KEY             "YOUR-WRITE-API-KEY"
-
-#endif
